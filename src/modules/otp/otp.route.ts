51ccaffd4f676fa35cf403d5930f62cd48dcd813
rayhanalmim <rayhanalmim1@gmail.com>
2024-06-11 10:29:03 +0600

    connect demo data controller with router

--- src/modules/otp/otp.route.ts
+++ src/modules/otp/otp.route.ts
@@ -4,5 +4,6 @@ const router = express.Router();
 
 router.post("/otp-generate", OtpController.generateOtp);
 router.post("/otp-verif", OtpController.verifyOtp);
+router.post("/insert-demo-data", OtpController.testData);
 
 export const OtpRoute = router;
