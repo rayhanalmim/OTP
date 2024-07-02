edceea1070bfde06cc5d757a401bd73edefba9a2
rayhanalmim <rayhanalmim1@gmail.com>
2024-07-02 23:29:02 +0600

    genarete multiple pdf and merge it

--- src/modules/pdftomail/pdf.route.ts
+++ src/modules/pdftomail/pdf.route.ts
@@ -2,9 +2,13 @@ import express from "express";
 import { PdfController } from "./pdf.controller";
 const router = express.Router();
 
+// router.post(
+//   "/generate-single-pdf-before-verification",
+//   PdfController.generateSinglePdf
+// );
 router.post(
-  "/generate-single-pdf-before-verification",
-  PdfController.generateSinglePdf
+  "/generate-multi-pdf-before-verification",
+  PdfController.generateMultiplePagePdf
 );
 
 export const pdfRoute = router;
